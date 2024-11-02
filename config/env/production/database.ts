fa96ed6f54bacbce4c1f1a29b25c107e4fe9c346
Marwan <saadmarwan16@gmail.com>
2024-11-02 20:38:38 +0300

    Dockerized the app

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -3,14 +3,11 @@ export default ({ env }) => ({
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "postgres"),
+      user: env("DATABASE_USERNAME", "strapi"),
       password: env("DATABASE_PASSWORD"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      ssl: env.bool("DATABASE_SSL", false),
     },
-    debug: false,
   },
 });
